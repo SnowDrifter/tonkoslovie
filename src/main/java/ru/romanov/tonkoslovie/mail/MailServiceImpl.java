b3c3612a7ff9359e837a756468f9e65377b23c39
Ivan <fl125@yandex.ru>
2017-07-21 01:57:41 +0300

    Update mail service

--- src/main/java/ru/romanov/tonkoslovie/mail/MailServiceImpl.java
+++ src/main/java/ru/romanov/tonkoslovie/mail/MailServiceImpl.java
@@ -1,9 +1,10 @@
 package ru.romanov.tonkoslovie.mail;
 
 import com.google.common.collect.Lists;
-import it.ozimov.springboot.templating.mail.model.Email;
-import it.ozimov.springboot.templating.mail.model.impl.EmailImpl;
-import it.ozimov.springboot.templating.mail.service.EmailService;
+import it.ozimov.springboot.mail.model.Email;
+import it.ozimov.springboot.mail.model.defaultimpl.DefaultEmail;
+import it.ozimov.springboot.mail.service.EmailService;
+import it.ozimov.springboot.mail.service.exception.CannotSendEmailException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,6 @@ import ru.romanov.tonkoslovie.user.entity.User;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
 import java.util.Date;
 import java.util.UUID;
 
@@ -32,7 +32,7 @@ public class MailServiceImpl implements MailService {
     }
 
     @Override
-    public void sendVerification(User user) throws UnsupportedEncodingException, AddressException {
+    public void sendVerification(User user) throws UnsupportedEncodingException, AddressException, CannotSendEmailException {
         String token = UUID.randomUUID().toString();
 
         EmailVerification verification = new EmailVerification();
@@ -41,12 +41,12 @@ public class MailServiceImpl implements MailService {
         verification.setExpiryDate(new Date());
         emailVerificationRepository.save(verification);
 
-        Email email = EmailImpl.builder()
+        Email email = DefaultEmail.builder()
                 .from(new InternetAddress("test@no-reply", "Test"))
                 .to(Lists.newArrayList(new InternetAddress(user.getEmail())))
                 .subject("Подтверждение регистрации")
                 .body(host + "/confirmRegistration?token=" + token)
-                .encoding(Charset.forName("UTF-8")).build();
+                .encoding("UTF-8").build();
 
         emailService.send(email);
     }
