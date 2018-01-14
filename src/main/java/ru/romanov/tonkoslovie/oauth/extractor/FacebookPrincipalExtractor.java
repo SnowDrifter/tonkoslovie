d9c92962ac6a3dbe257d767cac3ea3420ae79c46
Ivan <fl125@yandex.ru>
2018-01-15 02:51:35 +0300

    Add realization for FacebookPrincipalExtractor

--- src/main/java/ru/romanov/tonkoslovie/oauth/extractor/FacebookPrincipalExtractor.java
+++ src/main/java/ru/romanov/tonkoslovie/oauth/extractor/FacebookPrincipalExtractor.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.stereotype.Component;
 import ru.romanov.tonkoslovie.user.UserRepository;
+import ru.romanov.tonkoslovie.user.entity.Role;
+import ru.romanov.tonkoslovie.user.entity.User;
 
+import java.util.Collections;
 import java.util.Map;
+import java.util.UUID;
 
 @Component
 public class FacebookPrincipalExtractor implements PrincipalExtractor {
@@ -19,6 +23,21 @@ public class FacebookPrincipalExtractor implements PrincipalExtractor {
 
     @Override
     public Object extractPrincipal(Map<String, Object> map) {
-        return null;
+        User user;
+
+        if (userRepository.existsByEmail((String) map.get("email"))) {
+            user = userRepository.findFirstByEmail((String) map.get("email"));
+        } else {
+            user = new User();
+            user.setEmail((String) map.get("email"));
+            user.setFirstName((String) map.get("first_name"));
+            user.setFirstName((String) map.get("last_name"));
+            user.setRoles(Collections.singleton(Role.ROLE_USER));
+            user.setPassword(UUID.randomUUID().toString());
+            user.setEnabled(true);
+            userRepository.save(user);
+        }
+
+        return user;
     }
 }
