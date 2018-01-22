aed4a01a9063bce7e7d344fb19b36b2d8b79d47c
Ivan <fl125@yandex.ru>
2018-01-23 00:39:10 +0300

    Add metrics for jetty, update configs

---
+++ src/main/java/ru/romanov/tonkoslovie/jetty/JettyServletContainerCostumizer.java
@@ -0,0 +1,34 @@
+package ru.romanov.tonkoslovie.jetty;
+
+
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.jetty9.InstrumentedHandler;
+import org.eclipse.jetty.server.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JettyServletContainerCostumizer implements EmbeddedServletContainerCustomizer {
+
+    private final MetricRegistry metricRegistry;
+
+    @Autowired
+    public JettyServletContainerCostumizer(MetricRegistry metricRegistry) {
+        this.metricRegistry = metricRegistry;
+    }
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        JettyEmbeddedServletContainerFactory factory = (JettyEmbeddedServletContainerFactory) container;
+        factory.addServerCustomizers(this::customize);
+    }
+
+    private void customize(Server server) {
+        InstrumentedHandler handler = new InstrumentedHandler(metricRegistry, "jetty");
+        handler.setHandler(server.getHandler());
+        server.setHandler(handler);
+    }
+}
