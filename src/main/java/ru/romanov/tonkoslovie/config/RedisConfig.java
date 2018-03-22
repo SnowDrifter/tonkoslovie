3ffaff23c823ea7855c49fa35326d5f7501575eb
Ivan <fl125@yandex.ru>
2018-03-23 02:26:38 +0300

    Fix deprecated redis config

--- src/main/java/ru/romanov/tonkoslovie/config/RedisConfig.java
+++ src/main/java/ru/romanov/tonkoslovie/config/RedisConfig.java
@@ -3,6 +3,8 @@ package ru.romanov.tonkoslovie.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -20,14 +22,12 @@ public class RedisConfig {
 
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
-        jedisConnectionFactory.setDatabase(0);
-        jedisConnectionFactory.setHostName(redisHost);
-        jedisConnectionFactory.setPort(redisPort);
-        jedisConnectionFactory.setPassword(redisPassword);
-        jedisConnectionFactory.setUsePool(true);
-        jedisConnectionFactory.afterPropertiesSet();
-        return jedisConnectionFactory;
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(redisHost);
+        configuration.setPort(redisPort);
+        configuration.setPassword(RedisPassword.of(redisPassword));
+
+        return new JedisConnectionFactory(configuration);
     }
 
     @Bean
