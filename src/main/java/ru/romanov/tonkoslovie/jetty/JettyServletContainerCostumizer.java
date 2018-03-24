b27405ede671e771db402de5c14fc628947f8db0
Ivan <fl125@yandex.ru>
2018-03-24 03:14:20 +0300

    Fix jetty metrics

--- src/main/java/ru/romanov/tonkoslovie/jetty/JettyServletContainerCostumizer.java
+++ src/main/java/ru/romanov/tonkoslovie/jetty/JettyServletContainerCostumizer.java
@@ -1,35 +1,36 @@
-//package ru.romanov.tonkoslovie.jetty;
-//
-//
-//import com.codahale.metrics.MetricRegistry;
-//import com.codahale.metrics.jetty9.InstrumentedHandler;
-//import org.eclipse.jetty.server.Server;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-//import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-//import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
-//import org.springframework.stereotype.Component;
-//
-//TODO: fix imports or find alternative
-//@Component
-//public class JettyServletContainerCostumizer implements EmbeddedServletContainerCustomizer {
-//
-//    private final MetricRegistry metricRegistry;
-//
-//    @Autowired
-//    public JettyServletContainerCostumizer(MetricRegistry metricRegistry) {
-//        this.metricRegistry = metricRegistry;
-//    }
-//
-//    @Override
-//    public void customize(ConfigurableEmbeddedServletContainer container) {
-//        JettyEmbeddedServletContainerFactory factory = (JettyEmbeddedServletContainerFactory) container;
-//        factory.addServerCustomizers(this::customize);
-//    }
-//
-//    private void customize(Server server) {
-//        InstrumentedHandler handler = new InstrumentedHandler(metricRegistry, "jetty");
-//        handler.setHandler(server.getHandler());
-//        server.setHandler(handler);
-//    }
-//}
+package ru.romanov.tonkoslovie.jetty;
+
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.jetty9.InstrumentedHandler;
+import org.eclipse.jetty.server.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.embedded.jetty.JettyServerCustomizer;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JettyServletContainerCostumizer implements WebServerFactoryCustomizer<JettyServletWebServerFactory> {
+
+    private final MetricRegistry metricRegistry;
+
+    @Autowired
+    public JettyServletContainerCostumizer(MetricRegistry metricRegistry) {
+        this.metricRegistry = metricRegistry;
+    }
+
+    @Override
+    public void customize(JettyServletWebServerFactory server) {
+        JettyServerCustomizer customizer = new JettyServerCustomizer() {
+            @Override
+            public void customize(Server server) {
+                InstrumentedHandler handler = new InstrumentedHandler(metricRegistry, "jetty");
+                handler.setHandler(server.getHandler());
+                server.setHandler(handler);
+            }
+        };
+
+        server.addServerCustomizers(customizer);
+    }
+
+}
