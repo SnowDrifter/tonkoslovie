c5e63ff040510d8c56bd5b936b971405fce651d3
Ivan <fl125@yandex.ru>
2020-04-16 23:03:45 +0300

    Refactoring database config

--- src/main/java/ru/romanov/tonkoslovie/Application.java
+++ src/main/java/ru/romanov/tonkoslovie/Application.java
@@ -3,12 +3,10 @@ package ru.romanov.tonkoslovie;
 import it.ozimov.springboot.mail.configuration.EnableEmailTools;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 
 @EnableEmailTools
 @SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
         ThymeleafAutoConfiguration.class
 })
 public class Application {
