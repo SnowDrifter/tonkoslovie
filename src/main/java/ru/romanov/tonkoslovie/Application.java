6c90fa43ddcb41e97bd9a2f1dd8e912507a36552
Ivan <fl125@yandex.ru>
2020-04-28 22:27:22 +0300

    Refactoring Thymeleaf config

--- src/main/java/ru/romanov/tonkoslovie/Application.java
+++ src/main/java/ru/romanov/tonkoslovie/Application.java
@@ -2,11 +2,8 @@ package ru.romanov.tonkoslovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-        ThymeleafAutoConfiguration.class
-})
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/ru/romanov/tonkoslovie/config/WebMvcConfig.java
+++ src/main/java/ru/romanov/tonkoslovie/config/WebMvcConfig.java
@@ -1,51 +1,20 @@
 package ru.romanov.tonkoslovie.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.templatemode.TemplateMode;
 import ru.romanov.tonkoslovie.user.annotation.CurrentUserIdArgumentResolver;
 
 import java.util.List;
 
 @EnableWebMvc
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final CurrentUserIdArgumentResolver currentUserIdArgumentResolver;
-
-    @Autowired
-    public WebMvcConfig(CurrentUserIdArgumentResolver currentUserIdArgumentResolver) {
-        this.currentUserIdArgumentResolver = currentUserIdArgumentResolver;
-    }
-
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(thymeleafTemplateResolver());
-        return templateEngine;
-    }
-
-    @Bean
-    public SpringResourceTemplateResolver thymeleafTemplateResolver() {
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setPrefix("classpath:templates/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode(TemplateMode.HTML);
-        templateResolver.setCharacterEncoding("UTF-8");
-        return templateResolver;
-    }
-
-    @Bean
-    public RequestContextListener requestContextListener() {
-        return new RequestContextListener();
-    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
