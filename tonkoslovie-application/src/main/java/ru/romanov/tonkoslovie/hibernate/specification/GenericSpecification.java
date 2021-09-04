610a35bf288af4376352ac3e3bf374218b5ad890
Ivan Romanov <fl125@yandex.ru>
2021-09-04 18:59:00 +0300

    Refactoring specification builder

--- tonkoslovie-application/src/main/java/ru/romanov/tonkoslovie/hibernate/specification/GenericSpecification.java
+++ tonkoslovie-application/src/main/java/ru/romanov/tonkoslovie/hibernate/specification/GenericSpecification.java
@@ -1,12 +1,13 @@
 package ru.romanov.tonkoslovie.hibernate.specification;
 
 import lombok.AllArgsConstructor;
+import org.hibernate.query.criteria.internal.CriteriaBuilderImpl;
+import org.hibernate.query.criteria.internal.predicate.ComparisonPredicate;
+import org.hibernate.query.criteria.internal.predicate.LikePredicate;
 import org.springframework.data.jpa.domain.Specification;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
+import java.util.Date;
 
 @AllArgsConstructor
 public class GenericSpecification<T> implements Specification<T> {
@@ -15,22 +16,44 @@ public class GenericSpecification<T> implements Specification<T> {
 
     @Override
     public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+        Predicate predicate = createPredicate(root, builder);
+        return builder.and(predicate);
+    }
+
+    private Predicate createPredicate(Root<T> root,  CriteriaBuilder builder) {
+        Class<?> fieldType = root.get(criteria.getKey()).getJavaType();
+
+        if (fieldType == String.class) {
+            return new LikePredicate((CriteriaBuilderImpl) builder, root.get(criteria.getKey()), String.format("%%%s%%", criteria.getValue()));
+        } else {
+            Object value = convertValue(criteria.getValue(), fieldType);
+            ComparisonPredicate.ComparisonOperator operator = getCompressionOperator();
+            return builder.and(new ComparisonPredicate((CriteriaBuilderImpl) builder, operator, root.get(criteria.getKey()), value));
+        }
+    }
+
+    private ComparisonPredicate.ComparisonOperator getCompressionOperator() {
         switch (criteria.getOperation()) {
             case ">":
-                return builder.greaterThanOrEqualTo(root.get(criteria.getKey()), criteria.getValue());
+               return ComparisonPredicate.ComparisonOperator.GREATER_THAN;
             case "<":
-                return builder.lessThanOrEqualTo(root.get(criteria.getKey()), criteria.getValue());
-            case ":":
-                Class<?> fieldType = root.get(criteria.getKey()).getJavaType();
-
-                if (fieldType == String.class) {
-                    return builder.like(root.get(criteria.getKey()), String.format("%%%s%%", criteria.getValue()));
-                } else if (fieldType == boolean.class || fieldType == Boolean.class) {
-                    return builder.equal(root.get(criteria.getKey()), Boolean.valueOf(criteria.getValue()));
-                } else {
-                    return builder.equal(root.get(criteria.getKey()), criteria.getValue());
-                }
+                return ComparisonPredicate.ComparisonOperator.LESS_THAN;
+            default:
+                return ComparisonPredicate.ComparisonOperator.EQUAL;
         }
-        return null;
+    }
+
+    private Object convertValue(String value, Class<?> type) {
+        if (type == boolean.class || type == Boolean.class) {
+            return Boolean.valueOf(value);
+        } else if (type == Date.class) {
+            return new Date(Long.parseLong(value));
+        } else if (type == Long.class) {
+            return Long.valueOf(value);
+        } else if (type == Integer.class) {
+            return Integer.valueOf(value);
+        }
+
+        return value;
     }
 }
