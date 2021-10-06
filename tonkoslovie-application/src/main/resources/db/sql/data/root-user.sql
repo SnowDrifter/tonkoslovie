31d21148210e364491aef1f72dc90ba87a0e3f9c
Ivan Romanov <fl125@yandex.ru>
2021-10-06 21:33:00 +0300

    Change sequence for test user

--- tonkoslovie-application/src/main/resources/db/sql/data/root-user.sql
+++ tonkoslovie-application/src/main/resources/db/sql/data/root-user.sql
@@ -1,3 +1,3 @@
 INSERT INTO "user" (id, email, enabled, password, roles, username)
-VALUES (nextval('hibernate_sequence'), 'root@gmail.com', true,
+VALUES (nextval('user_id_sequence'), 'root@gmail.com', true,
         '$2a$11$iMJtVcPBs7o75fDn0pCrte0emi2tacaDvsgUziE4B2AiJoNAGKTdG', 'ROLE_USER,ROLE_ADMIN', 'root');
